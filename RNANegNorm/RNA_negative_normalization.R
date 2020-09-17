fa4fba03b46e66b2fa017bcd3e3c4dd6c0aa9e30
NicoleEO <Nicole.Ortogero@gmail.com>
2020-09-17 02:19:26 -0700

    Initial negative normalization code

---
+++ RNANegNorm/RNA_negative_normalization.R
@@ -0,0 +1,29 @@
+### functions to perform negative normalization:
+# note: this code assumes the input is QC'd RNA counts collapsed by target
+
+
+
+# main function called by DSP-DA:
+main <- function(dataset, segmentAnnotations, targetAnnotations, outputFolder) {
+  pools <- unique(targetAnnotations[["ProbePool"]])
+  pool_neg_norm <- lapply(pools, 
+    function(pool) {
+      # Get pool and corresponding target counts
+      pool_neg <- 
+        targetAnnotations[targetAnnotations[["CodeClass"]] == "Negative" & 
+                            targetAnnotations[["ProbePool"]] == pool, "TargetName"]
+      pool_targets <- 
+        targetAnnotations[targetAnnotations[["ProbePool"]] == pool, "TargetName"]
+
+      # Calculate normalization factor and normalized counts
+      pool_neg_factors <- 
+        unlist(dataset[pool_neg, ] / geoMean(as.numeric(dataset[pool_neg, ])))
+      pool_counts <-
+        as.matrix(dataset[pool_targets, ]) %*% diag(1 / pool_neg_factors)
+   })
+
+  #Collapse data back into one data frame
+  neg_norm_df <- data.frame(do.call(rbind, pool_neg_norm))
+  colnames(neg_norm_df) <- colnames(dataset)
+  return(neg_norm_df)
+}
