32d487de4cce9d516fa60c484ddc161fcadb2c9a
Jason Reeves <jreeves@nanostring.com>
2020-11-12 10:52:44 -0800

    Update DimReduction.R
    add copyright and contact information

--- DimReduction/DimReduction.R
+++ DimReduction/DimReduction.R
@@ -29,6 +29,16 @@ color_levels = c("PanCK-pos", "PanCK-neg")
 # be alphabetical unless values of color_by are provided
 # in color_levels, in which case that order shall be used
 
+# Copyright (C) 2020, NanoString Technologies, Inc.
+#    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License along with this program.  If not, see https://www.gnu.org/licenses/.
+# Contact us: 
+#   NanoString Technologies, Inc.
+#   530 Fairview Avenue N
+#   Seattle, WA 98109
+# Tel: (888) 358-6266
+
 ##############################
 #        Execution Code      # 
 ##############################
