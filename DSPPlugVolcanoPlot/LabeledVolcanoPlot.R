e50cb1d484dbd06aab2d70c24ba0389fa04834fd
Maddy Griswold <40255151+maddygriz@users.noreply.github.com>
2020-11-24 15:48:50 -0800

    beta testing rd 2

--- DSPPlugVolcanoPlot/LabeledVolcanoPlot.R
+++ DSPPlugVolcanoPlot/LabeledVolcanoPlot.R
@@ -24,6 +24,9 @@ negative_label <- "50 um Circle"
 # Positive (right) label for Fold Change from volcano plot
 positive_label <- "200 um Circle"
 
+# Show color legend on figure
+show_legend <- TRUE
+
 # Number of genes to label
 #   gene_list overrides this variable if set
 n_genes <- 25
@@ -35,20 +38,20 @@ gene_list <- NULL #c("IL2RG", "GLUL", "SPIB", "C2")
 
 ####################### THRESHOLDS #######################
 # P-value threshold, must set fdr_thresh to NULL to use
-pval_thresh <- NULL
+pval_thresh <- 0.05
 
 # FDR threshold, default threshold over pval_thresh
 fdr_thresh <- 0.01
 
-# Fold Change threshold 
-fc_thresh <- 0.75
+# Fold Change threshold, if set coloring options will change
+fc_thresh <- 0.5
 
-# Should threshold lines be added to plot
-thresh_lines <- TRUE
+# Should genes below fc_thresh be labeled
+label_fc <- FALSE
 
 ######################### FONTS ##########################
 # Font Size
-font_size <- 12
+font_size <- 8
 
 # Font Family
 #   options include: serif, sans, mono
@@ -62,6 +65,15 @@ plot_width <- 6
 plot_height <- 4
 
 ####################### COLORING #########################
+# Default point color for points not in target groups or
+#   above thresholds
+default_color <- "grey25"
+
+# Color of points below fc_thresh but above pval and/or 
+#   fdr thresholds. change to the same as default_color
+#   if you don't want these targets called out
+fc_color <- "grey50"
+
 # Specific target groups to color in the plot
 #   If variable is set, genes are colored by given target 
 #     group; else genes colored if they are above thresholds
@@ -69,9 +81,10 @@ target_groups <- NULL #c("Hemostasis", "DNA Repair")
 
 # Color options for plot. Must have more colors than
 #   number of target groups 
-color_options <- c("#3A6CA1", "#FFD861", "#CF4244", "#47BAB4", 
-                   "#474747", "#EB739E", "#318026", "#A66293", 
-                   "#F28E2B", "#8F6954")
+color_options <-  c("#3A6CA1", "#FFD861", "#CF4244", 
+                    "#47BAB4", "#474747", "#EB739E", 
+                    "#318026", "#A66293", "#F28E2B", 
+                    "#8F6954")
 
 ##########################################################
 #### end of arguments. DO NOT CHANGE CODE BELOW HERE  ####
@@ -123,7 +136,7 @@ main <- function(dataset, segmentAnnotations, targetAnnotations, outputFolder){
   # create volcano plot
   returns <- volcanoPlot(de=de_results)
   
-  ggsave(filename=paste0(plot_title, "_volcano_plot.", output_format),
+  ggsave(filename=paste0("volcano_plot_", plot_title, ".", output_format),
          plot=returns$plot,
          device=output_format,
          path=outputFolder,
@@ -133,7 +146,7 @@ main <- function(dataset, segmentAnnotations, targetAnnotations, outputFolder){
   
   write.table(x=returns$gene_labels, 
               file=file.path(outputFolder,
-                               paste0(plot_title, "_labeled_genes.txt"), 
+                               paste0("labeled_genes_", plot_title, ".txt"), 
                                fsep=.Platform$file.sep),
               sep="\t", quote=FALSE, row.names=FALSE)
 } 
@@ -150,6 +163,11 @@ volcanoPlot <- function(de){
   maxFC <- max(abs(de$Log2))
   
   maxPval <- min(de$Pvalue)
+  
+  # find closest FDR value to fdr_thresh and use that pvalue to add y axis cutoff line
+  if(!is.null(fdr_thresh)){
+    fdr_pval <- mean(de$Pvalue[which(abs(de$FDR - fdr_thresh) == min(abs(de$FDR - fdr_thresh)))])
+  }
  
   # function to return pvalue on y axis rather than -log10(pvalue)
   # copied from 360Report VolcanoPlot.R
@@ -166,18 +184,23 @@ volcanoPlot <- function(de){
   
   # create basic volcano plot with correct formatting
   gp <- ggplot(de, aes(x=Log2, y=Pvalue))+
-    geom_point(color="grey60")+
-    labs(y="Significance, Pvalue",
+    geom_point(color=default_color)+
+    labs(y="Pvalue",
          x=paste(negative_label, "<-", "log2(FC)", "->", positive_label, sep=" "),
          title=plot_title)+
-    theme(text=element_text(size=font_size, family=font_family), 
-          axis.line=element_line(size=1))+
     theme_bw()+
-    scale_x_continuous(limits=c(-maxFC, maxFC))+
-    scale_y_continuous(trans=revlog_trans(base=10),
-                       labels=function(x) format(x, trim=TRUE, digits=4,
-                                                 scientific=ifelse(maxPval < 0.0001, TRUE, FALSE), 
-                                                 drop0trailing=TRUE))  
+    theme(text=element_text(size=font_size, family=font_family))+
+    scale_x_continuous(limits=c(-maxFC, maxFC))
+  
+    # this makes for easier testing if not running in DSPDA, will flip yaxis of graph
+    # scale_y_continuous(trans=revlog_trans(base=10),
+    #                    labels=function(x) format(x, trim=TRUE, digits=4,
+    #                                              scientific=ifelse(maxPval < 0.0001, TRUE, FALSE), 
+    #                                              drop0trailing=TRUE)) 
+  
+  if(show_legend == FALSE){
+    gp <- gp + theme(legend.position="none")
+  }
   
   # subset de to only include genes either in specified target groups or above pval/fdr threshold
   if(!is.null(target_groups)){
@@ -195,58 +218,127 @@ volcanoPlot <- function(de){
     
     color_label <- "Target Group\nMembership"
   }else{
-    if(is.null(fdr_thresh)){
-      gene_coloring <- de[which(de$Pvalue < pval_thresh),]
-      label_thresh <- paste("pval <", pval_thresh)
+    color_options <- color_options[1:2]
+    if(!is.null(fdr_thresh) & !is.null(pval_thresh)){
+      if(fdr_pval < pval_thresh){
+        gene_coloring <- de[which(de$Pvalue < pval_thresh),]
+        label_thresh_low <- paste("pval <", pval_thresh)
+        label_thresh_high <- paste("FDR <", fdr_thresh)
+        high_thresh <- fdr_pval
+      }else{
+        gene_coloring <- de[which(de$Pvalue < fdr_pval),]
+        label_thresh_low <- paste("FDR <", fdr_thresh)
+        label_thresh_high <- paste("pval <", pval_thresh)
+        high_thresh <- pval_thresh
+      }
+      
+      # label points as positive or negative FC
+      gene_coloring$Target_coloring <- ifelse(test=gene_coloring$Log2 < 0, 
+                                              yes=negative_label, 
+                                              no=positive_label)
+      
+      # label points as above pval or FDR threshold
+      gene_coloring$Target_coloring <- ifelse(test=gene_coloring$Pvalue > high_thresh, 
+                                              yes=paste(label_thresh_low, gene_coloring$Target_coloring), 
+                                              no=paste(label_thresh_high, gene_coloring$Target_coloring))
+      
+      # make color options have muted colors for higher threshold
+      color_options <- c(color_options, muted(color_options, l=80))
+      names(color_options) <- c(paste(label_thresh_high, negative_label), 
+                                paste(label_thresh_high, positive_label),
+                                paste(label_thresh_low, negative_label), 
+                                paste(label_thresh_low, positive_label))
     }else{
-      gene_coloring <- de[which(de$FDR < fdr_thresh),]
-      label_thresh <- paste("FDR <", fdr_thresh)
+      if(is.null(fdr_thresh)){
+        gene_coloring <- de[which(de$Pvalue < pval_thresh),]
+        label_thresh <- paste("pval <", pval_thresh)
+      }else{
+        gene_coloring <- de[which(de$FDR < fdr_thresh),]
+        label_thresh <- paste("FDR <", fdr_thresh)
+      }
+      gene_coloring$Target_coloring <- ifelse(test=gene_coloring$Log2 < 0, 
+                                              yes=paste(label_thresh, negative_label), 
+                                              no=paste(label_thresh, positive_label))
+      
+      names(color_options) <- c(paste(label_thresh, negative_label), 
+                                paste(label_thresh, positive_label))
     }
     
-    gene_coloring$Target_coloring <- ifelse(test=gene_coloring$Log2 < 0, 
-                                            yes=negative_label, 
-                                            no=positive_label)
+    color_label <- "Significance:"
     
-    color_label <- paste("Significance:", label_thresh, sep="\n")
+    # color by fc_thresh if fc_thresh is not NULL
+    if(!is.null(fc_thresh)){
+      gene_coloring$Target_coloring[abs(gene_coloring$Log2) < fc_thresh] <- paste("FC <", fc_thresh)
+      color_options <- c(color_options, fc_color)
+      names(color_options)[length(color_options)] <- paste("FC <", fc_thresh)
+    }
   }
+  
+  color_options <- c(color_options, default_color)
+  names(color_options)[length(color_options)] <- "Not Specified"
   
   # add coloring to ggplot
   gp <- gp + geom_point(data=gene_coloring, aes(x=Log2, y=Pvalue, color=Target_coloring))+
     labs(color=color_label)+
     scale_color_manual(values=color_options)
   
-  # add threshold lines if specified
-  if(thresh_lines == TRUE){
+  # add threshold line values to y axis
+  yaxis <- data.frame(brk=as.numeric(pretty_breaks(n=4)(0:max(de$X.log10.pvalue))))
+  yaxis$brk <- 10^-(yaxis$brk)
+  
+  # keep scientific notation if small enough pvalues when changing to character
+  yaxis$label <- format(yaxis$brk, trim=TRUE, digits=4,
+                        scientific=ifelse(maxPval < 0.0001, TRUE, FALSE), 
+                        drop0trailing=TRUE)
+  
+  # add threshold lines if thresholds are not NULL
+  if(!is.null(fc_thresh)){
     gp <- gp + geom_vline(xintercept=fc_thresh, linetype="dotted")+
-      geom_vline(xintercept=-fc_thresh, linetype="dotted")
-    
-    if(is.null(pval_thresh)){
-      # find closest FDR value to fdr_thresh and use that pvalue to add y axis cutoff line
-      fdr_pval <- mean(de$Pvalue[which(abs(de$FDR - fdr_thresh) == min(abs(de$FDR - fdr_thresh)))])
-      gp <- gp + geom_hline(yintercept=fdr_pval, linetype="dotted")
-        #geom_text(aes(-maxFC, fdr_pval, label=paste("FDR =", fdr_thresh), vjust=1, hjust=-1), size=3)
-    }else{
-      gp <- gp + geom_hline(yintercept=pval_thresh, linetype="dotted")
-        #geom_text(aes(maxFC, pval_thresh, label=paste("FDR =", pval_thresh), vjust=1, hjust=1), size=3)
-    }
+      geom_vline(xintercept=-fc_thresh, linetype="dotted")+
+      annotate("text", x=fc_thresh+0.35, y=1,family=font_family, size=font_size/3,
+               label=paste0("FC=", round(2^fc_thresh, digits=2)))
   }
+  if(!is.null(pval_thresh)){
+    gp <- gp + geom_hline(yintercept=pval_thresh, linetype="dotted")
+    yaxis <- rbind(yaxis, c(pval_thresh, paste0('pval=',pval_thresh)))
+  }
+  if(!is.null(fdr_thresh)){
+    gp <- gp + geom_hline(yintercept=fdr_pval, linetype="dotted")
+    yaxis <- rbind(yaxis, c(fdr_pval, paste0('FDR=',fdr_thresh)))
+  }
+  
+  # order yaxis in increasing value 
+  yaxis$brk <- as.numeric(yaxis$brk)
+  yaxis <- yaxis[order(yaxis$brk, decreasing=F),]
   
   # subset de to only contain genes to label on plot, either by user specified genes or top n_genes by pval
   if(!is.null(gene_list)){
     gene_labels <- subset(de, subset=Target.Name %in% gene_list)
   }else{
-    gene_labels <- de[head(order(abs(de$Pvalue), decreasing=FALSE), n=n_genes),]
+    # remove gene labels for genes below lowest pvalue cutoff
+    gene_labels <- de[which(de$Pvalue < min(fdr_pval, pval_thresh)),]
+    
+    # only label genes above fc_thresh if set by user else only look at pvalue
+    if(!is.null(fc_thresh) & label_fc == FALSE){
+      gene_labels <- gene_labels[which(abs(gene_labels$Log2) > fc_thresh),]
+    }
+    
+    # only keep top # of genes by pvalue
+    gene_labels <- gene_labels[head(order(gene_labels$Pvalue, decreasing=FALSE), n=n_genes),]
   }  
   
   # add gene labels to ggplot
   gp <- gp + geom_text_repel(data=gene_labels, aes(x=Log2, y=Pvalue, label=Target.Name), 
-                             family=font_family, force=5,
-                             size=max(min(font_size*min(5/nrow(gene_labels), 5.5), 5.5), 3))
+                             family=font_family, force=5, fontface="bold", min.segment.length=0.1,
+                             size=max(min(font_size*min(5/nrow(gene_labels), 5.5), 4), 3))
+  
+  # add y axis labels 
+  gp <- gp + scale_y_continuous(trans=revlog_trans(base=10), breaks=as.numeric(yaxis$brk),
+                          labels=yaxis$label)
 
   colnames(gene_labels) <- c("Target tag", "Target group memership/s", "Target Name", "Log2", 
                              "Pvalue", "Adjusted pvalue", "-log10 pvalue", "-log10 adjusted pvalue",
                              "FDR")
-  
   
   returns <- list(gp, gene_labels)
   names(returns) <- c("plot", "gene_labels")
@@ -320,11 +412,6 @@ testVariableFormats <- function(de=de_results){
                            object_name=character_variables[v], 
                            class_name="character")
     }
-  }
-  
-  # check that thresh_lines is a logical or NULL
-  if(!is.null(thresh_lines)){
-    expectIdenticalClass(object=thresh_lines, object_name="thresh_lines", class_name="logical")
   }
   
   # check that either n_genes or gene_list is not NULL
