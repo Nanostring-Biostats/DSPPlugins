4bdd2e624e830b3f6ac6469465b024886089019e
karagorman <karagorman@yahoo.com>
2023-05-15 21:56:09 +0000

    update gini coefficient and shannon diversity functions

--- TCRAnalysis/TCR_Analysis.R
+++ TCRAnalysis/TCR_Analysis.R
@@ -342,120 +342,40 @@ tcr <- function(dataset, outputFolder) {
 } 
   
   
+#' calculate_gini_coefficient
+#'
+#' Function to calculate gini coefficient
+#' @param x a numeric vector containing non-negative values
+#' @return gini_coeff
+#' @export
+calculate_gini_coefficient <- function(x) {
   
+  weights <- rep(1/length(x), length(x))
   
+  x <- x[order(x)]
+  p <- cumsum(weights)
+  n <- length(x)
+  nu <- cumsum(weights*x)
+  n <- length(nu)
+  nu <- nu/nu[n]
   
-  #'From dineq package
-  #' @title Gini coefficient
-  #'
-  #' @description Returns the (optional weighted) Gini coefficient for a vector.
-  #'
-  #' @details The Gini coefficient is a measure of inequality among values of a distribution. The most
-  #' used single measure for income inequality. The coefficient can theoretically range between 0 and
-  #' 1, with 1 being the highest possible inequality (for instance: 1 person in a society has all
-  #' income; the others none). But coefficients that are negative or greater than 1 are also possible
-  #' because of negative values in the distribution. Compared to other measures of inequality,
-  #' the Gini coefficient is especially sensitive for changes in the middle of the distribution.
-  #'
-  #' Extension of the gini function in reldist package in order to handle missings.
-  #'
-  #' @param x a numeric vector containing at least non-negative elements.
-  #' @param weights an optional vector of weights of x to be used in the computation of the Gini
-  #' coefficient. Should be NULL or a numeric vector.
-  #'
-  #' @return The value of the Gini coefficient.
-  #'
-  #' @examples
-  #' #calculate Gini coefficient using Mexican Income data set
-  #' data(mex_inc_2008)
-  #'
-  #' #unweighted Gini coefficient:
-  #' gini.wtd(mex_inc_2008$income)
-  #'
-  #' #weighted Gini coefficient:
-  #' gini.wtd(x=mex_inc_2008$income, weights=mex_inc_2008$factor)
-  #'
-  #' @source Handcock, M. (2016), Relative Distribution Methods. Version 1.6-6. Project home page
-  #' at http://www.stat.ucla.edu/~handcock/RelDist.
-  #'
-  #' @references
-  #' Haughton, J. and S. Khandker. (2009) \emph{Handbook on poverty and inequality},
-  #' Washington, DC: World Bank.
-  #'
-  #' Cowell F. (2000) Measurement of Inequality. In Atkinson A. and Bourguignon F. (eds.) \emph{
-  #' Handbook of Income Distribution}. Amsterdam: Elsevier, p. 87-166.
-  #'
-  #' @export
+  gini_coeff <- sum(nu[-1] * p[-n]) - sum(nu[-n] * p[-1])
   
-  gini.wtd <- function (x, weights = NULL)
-  {
-    if (is.null(weights)){
-      weights <- rep(1, length(x))}
-    
-    missing <- !(is.na(x) | is.na(weights))
-    x <- x[missing]
-    weights <- weights[missing]
-    if (!all(weights>=0)) stop("At least one weight is negative", call.=FALSE)
-    if (all(weights == 0)) stop("All weights are zero", call.=FALSE)
-    weights <- weights/sum(weights)
-    
-    order <- order(x)
-    x <- x[order]
-    weights <- weights[order]
-    p <- cumsum(weights)
-    nu <- cumsum(weights * x)
-    n <- length(nu)
-    nu <- nu/nu[n]
-    gini <- sum(nu[-1] * p[-n]) - sum(nu[-n] * p[-1])
-    return (gini)
-  }
+  return(gini_coeff)
+}
+
+#' calculate_shannon_diversity
+#' 
+#' Function to calculate shannon diversity
+#' @param x a numeric matrix containing non-negative values
+#' @return H vector of shannon diversity scores
+#' @export
+calculate_shannon_diversity <- function(x) {
   
-  # From vegan package
-  `diversity` <-
-    function (x, index = "shannon", groups, equalize.groups = FALSE,
-              MARGIN = 1, base = exp(1))
-    {
-      x <- drop(as.matrix(x))
-      if (!is.numeric(x))
-        stop("input data must be numeric")
-      if (any(x < 0, na.rm = TRUE))
-        stop("input data must be non-negative")
-      ## sum communities for groups
-      if (!missing(groups)) {
-        if (MARGIN == 2)
-          x <- t(x)
-        if (length(groups) == 1) # total for all SU
-          groups <- rep(groups, NROW(x))
-        if (equalize.groups)
-          x <- decostand(x, "total")
-        x <- aggregate(x, list(groups), sum) # pool SUs by groups
-        rownames(x) <- x[,1]
-        x <- x[,-1, drop=FALSE]
-        if (MARGIN == 2)
-          x <- t(x)
-      }
-      INDICES <- c("shannon", "simpson", "invsimpson")
-      index <- match.arg(index, INDICES)
-      if (length(dim(x)) > 1) {
-        total <- apply(x, MARGIN, sum)
-        x <- sweep(x, MARGIN, total, "/")
-      } else {
-        x <- x/(total <- sum(x))
-      }
-      if (index == "shannon")
-        x <- -x * log(x, base)
-      else
-        x <- x*x
-      if (length(dim(x)) > 1)
-        H <- apply(x, MARGIN, sum, na.rm = TRUE)
-      else
-        H <- sum(x, na.rm = TRUE)
-      if (index == "simpson")
-        H <- 1 - H
-      else if (index == "invsimpson")
-        H <- 1/H
-      ## check NA in data
-      if (any(NAS <- is.na(total)))
-        H[NAS] <- NA
-      H
-    }
+  total_counts <- apply(x, 1, sum)
+  x <- sweep(x, 1, total_counts, "/")
+  x <- -x * log(x, exp(1))
+  H <- apply(x, 1, sum, na.rm = TRUE)
+  
+  return(H)
+}
