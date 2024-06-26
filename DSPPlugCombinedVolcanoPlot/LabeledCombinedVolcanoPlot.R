bf4bb46fd4ec952f4b12ef2e92f7c5e5c34a3716
Rona Vitancol <rvitancol@nanostring.com>
2024-06-25 17:55:09 -0700

    license

--- DSPPlugCombinedVolcanoPlot/LabeledCombinedVolcanoPlot.R
+++ DSPPlugCombinedVolcanoPlot/LabeledCombinedVolcanoPlot.R
@@ -103,15 +103,12 @@ color_by_dataset <- TRUE
 #### End of User Inputs. PLEASE DO NOT CHANGE CODE BELOW HERE  ####
 ##########################################################
 # MIT License
-# Copyright 2020 Nanostring Technologies, Inc.
+# Copyright 2024 Bruker Spatial Biology, Inc.
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# Contact us: 
-#   NanoString Technologies, Inc.
-#   530 Fairview Avenue N
-#   Seattle, WA 98109
-# Tel: (888) 358-6266
+# Contact us: support@nanostring.com 
+
 ##############################
 
 ##############################
